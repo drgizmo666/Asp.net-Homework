f6ef57449801a2e627d8cd68c5d64fe268726049
drgizmo666 <james_e_powell@live.com>
2016-02-04 15:42:56 -0800

    add lab 4
    added what i have for lab 4

---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+This is where my asp.net homework is stored
---
+++ lab 4/CentralCity/CentralCity/Models/Topic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CentralCity.Models
+{
+    public class Topic
+    {
+        public int TopicID { get; set; }
+        public int MessageID { get; set; }
+        List<Message> messages = new List<Message>();
+        public string TopicName { get; set; }
+        public List<Message> Messages
+        {
+            get { return messages; }
+        }
+    }
+}
